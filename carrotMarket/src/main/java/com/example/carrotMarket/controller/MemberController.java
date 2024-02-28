c844534ea026c57e13808f48c87220cd8401dec0
Lee <94841127+galaxian@users.noreply.github.com>
2024-02-28 20:45:55 +0900

    feat: 내 프로필 조회 컨트롤러 로직 구현

--- carrotMarket/src/main/java/com/example/carrotMarket/controller/MemberController.java
+++ carrotMarket/src/main/java/com/example/carrotMarket/controller/MemberController.java
@@ -1,7 +1,11 @@
 package com.example.carrotMarket.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.carrotMarket.dto.MemberResDto;
 import com.example.carrotMarket.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -12,4 +16,12 @@ public class MemberController {
 
 	private final MemberService memberService;
 
+	@GetMapping("/user-info/{id}")
+	public ResponseEntity<MemberResDto> getMyProfile(@PathVariable("id") Long id) {
+
+		// todo 로그인 구현 후 추가 로직 필요
+
+		MemberResDto resDto = memberService.getMyProfile(id);
+		return ResponseEntity.ok(resDto);
+	}
 }
