23c28b6a3bcad587613b57a4b35e34244a8aecb4
Lee <94841127+galaxian@users.noreply.github.com>
2024-02-28 21:20:10 +0900

    feat: member entity no arg 생성자 추가

--- carrotMarket/src/main/java/com/example/carrotMarket/entity/member/Member.java
+++ carrotMarket/src/main/java/com/example/carrotMarket/entity/member/Member.java
@@ -3,9 +3,12 @@ package com.example.carrotMarket.entity.member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member {
     @Id@GeneratedValue
