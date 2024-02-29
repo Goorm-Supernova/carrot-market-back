8c2c895b7d1fae0b4eb622069ad78126703e1396
geon8692 <geon4735@gmail.com>
2024-02-29 16:14:28 +0900

    Feat: Post, Img 엔티티 연관관계 설정 및 연관관계 편의 메서드 추가

--- carrotMarket/src/main/java/com/example/carrotMarket/entity/img/Img.java
+++ carrotMarket/src/main/java/com/example/carrotMarket/entity/img/Img.java
@@ -1,16 +1,18 @@
 package com.example.carrotMarket.entity.img;
 
 import com.example.carrotMarket.entity.post.Post;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Img {
     @Id@GeneratedValue
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
     private Post post;
     private String src;
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
--- carrotMarket/src/main/java/com/example/carrotMarket/entity/post/Post.java
+++ carrotMarket/src/main/java/com/example/carrotMarket/entity/post/Post.java
@@ -1,9 +1,13 @@
 package com.example.carrotMarket.entity.post;
 
+import com.example.carrotMarket.entity.img.Img;
 import com.example.carrotMarket.entity.member.Member;
 import com.example.carrotMarket.enums.Status;
 import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,7 +22,15 @@ public class Post {
     private String contents;
     private Status status;
 
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Img> images = new ArrayList<>();
+
     public void setMember(Member member) {
         this.member = member;
+    }
+
+    public void addImage(Img image) {
+        images.add(image);
+        image.setPost(this);
     }
 }
